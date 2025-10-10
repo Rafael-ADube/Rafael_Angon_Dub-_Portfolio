b7c4e8cafc0975058dda9b20975fb7885bb50fa7
Rafael-ADube <124070431+Rafael-ADube@users.noreply.github.com>
2025-10-10 17:40:40 -0400

    création pageprojet3

--- web_portfolio/script.js
+++ web_portfolio/script.js
@@ -45,34 +45,13 @@ document.querySelectorAll(".row.align-items-center").forEach((section, i) => {
       });
     }
   });
-// TEST ANIMATION LE PROTFOLIO
-  // Animation du paragraphe
-  gsap.from(".hero p", {
-    scrollTrigger: {
-      trigger: ".hero",
-      start: "top 80%",
-    },
-    opacity: 0,
-    y: 50,
-    duration: 1,
-    delay: 0.4,
-    ease: "power2.out"
-  });
-    const h1 = document.querySelector("#hero-text h1");
-  const letters = h1.textContent.split("");
-  h1.textContent = "";
-  letters.forEach(char => {
-    const span = document.createElement("span");
-    span.textContent = char;
-    span.classList.add("char");
-    h1.appendChild(span);
-  });
+
 
   // Timeline GSAP
   const tl = gsap.timeline({defaults: {duration: 0.6, ease: "power3.out"}});
 
   tl.to(".char", {opacity: 1, y: 0, stagger: 0.05})
-    .from("#hero-text p", {opacity: 0, y: 20, duration: 1}, "-=0.3");
+    .from("#hero-text p", {opacity: 0, y: 20, duration: 1}, "0.3");
 
 // COMPTEURS DE COMPÉTENCES AVEC VUE.JS ET GSAP
 const app = Vue.createApp({
@@ -92,24 +71,39 @@ const app = Vue.createApp({
   },
   methods: {
     startAnimation() {
-      this.programs.forEach(prog => {
-        prog.current = 0;
-        const step = Math.ceil(prog.level / 50); // vitesse (plus petit = plus lent)
-        const interval = setInterval(() => {
-          if (prog.current < prog.level) {
-            prog.current += step;
-            if (prog.current > prog.level) prog.current = prog.level;
-          } else {
-            clearInterval(interval);
+      this.programs.forEach((program, index) => { 
+        gsap.to(program, {
+          current: program.level,
+          duration: 2,
+          delay: index * 0.3,
+          ease: "power1.out",
+          onUpdate: () => {
+            program.current = Math.round(program.current);
           }
-        }, 40); // intervalle en ms
-      });
+        });
+      }
+      );
     }
   },
-  mounted() {
-    // Démarre l'animation au chargement de la page
-    this.startAnimation();
-  }
+mounted() {
+  ScrollTrigger.create({
+    trigger: "#skills-section",
+    start: "top 80%",
+    onEnter: () => this.startAnimation()
+  });
+}
 });
 
 app.mount('#skills-section');
+gsap.from("#skills-section img", {
+  scrollTrigger: {
+    trigger: "#skills-section",
+    start: "top 90%",
+  },
+  opacity: 0,
+  scale: 0.8,
+  duration: 1,
+  stagger: 0.2,
+  ease: "power2.out"
+});
+
